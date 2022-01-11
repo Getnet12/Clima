04b846d7fa1f86ef790f1b008e77b9c3f8c4f812
Getnet Mekuriyaw <melkamugetnet1@gmail.com>
2022-01-11 01:22:53 -0600

    Worked with JSON decoder

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct WeatherManger{
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=aa79077f43896452ae50a7a942b97492"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=aa79077f43896452ae50a7a942b97492&units=metric"
     
     func fetchWeather(cityName: String ){
         let urlString = "\(weatherURL)&q=\(cityName)"
@@ -30,15 +30,25 @@ struct WeatherManger{
                 }
                 
                 if let safeData = data {
-                    let dataString = String(data: safeData, encoding: .utf8)
-                    print(dataString)
+//                    let dataString = String(data: safeData, encoding: .utf8)
+//                    print(dataString)
+                    // always use self when calling method inside the closure
+                    self.parseJson(weatherData: safeData)
                 }
             }
             //4. Start the task
             task.resume()
         }
     }
-    
-    
+    func parseJson(weatherData: Data){
+        
+        let decoder = JSONDecoder()
+        do {
+        let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
+    }
     
 }
